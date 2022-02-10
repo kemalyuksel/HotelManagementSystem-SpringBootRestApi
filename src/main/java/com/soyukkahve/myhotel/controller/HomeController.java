d810c377894d4b611df47831d2bd8015115a9b2b
ibrahim <ibrahim.sak@volantx.com>
2022-02-10 20:24:41 +0300

    add home controller metot

--- src/main/java/com/soyukkahve/myhotel/controller/HomeController.java
+++ src/main/java/com/soyukkahve/myhotel/controller/HomeController.java
@@ -1,11 +1,14 @@
 package com.soyukkahve.myhotel.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-
-
+    @RequestMapping("/home")
+    public String index() {
+        return "Hello";
+    }
 
 
 }
