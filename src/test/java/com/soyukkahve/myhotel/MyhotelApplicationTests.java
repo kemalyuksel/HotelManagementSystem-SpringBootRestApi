5a61a79adde6cbfed31d88ba521d57f41cccfcd0
Kemal Yuksel <kemalyksell98@gmail.com>
2022-02-10 17:48:53 +0300

    initial commit

---
+++ src/test/java/com/soyukkahve/myhotel/MyhotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soyukkahve.myhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
